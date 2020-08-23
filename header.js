239f364555b1ca82c3b036b1612caf9c09feafcb
Herokun <me@example.com>
2020-08-23 17:58:45 +0900

    2020年度の特設サイト関係の案内を追加

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 document.write('<header>');
-document.write('<h1><a href="/index.html"><img src="/img/logo.png" alt="海城コンピューター部" height="70"></a></h1>');
+document.write('<h1><a href="/"><img src="/img/logo.png" alt="海城コンピューター部" height="70"></a></h1>');
 document.write('<h2>海城コンピューター部の公式ページです。</h2>');
 document.write('<p>自作ゲームの配布や活動の公開を行っています。</p>');
 document.write('</header>');
